6d11e3efe685815d8612d06c9d45fd88f7f0c1da
ss210344 <ss210344@reigate.ac.uk>
2021-12-01 11:31:35 +0000

    put on school computer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,28 +10,22 @@ import java.util.ArrayList;
 
 //myObj change the name and add try catch
 public class Main {
-    private static File libary = new File("books.txt");
+    private static File library = new File("books.txt");
+    private static ArrayList<ArrayList<String>> books = new ArrayList<>();
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         CreateFile();
-        ArrayList<ArrayList<String>> books = new ArrayList<>();
-        books = addBooks(books);
-        WriteToFile(books);
-        ReadFile();
-        System.out.println("Do you want to delete this file now Y or N?");
-        String userOption = userInput();
-        if (userOption.equals("Y")) {
-            DeleteFile();
-        }
+        mainMenu();
+
 
 
     }
-//creates or finds the file
+    //creates or finds the file
     public static void CreateFile() {
         try {
-            if (libary.createNewFile()) {
-                System.out.println("File created: " + libary.getName());
+            if (library.createNewFile()) {
+                System.out.println("File created: " + library.getName());
             } else {
                 System.out.println("File already exists.");
             }
@@ -40,13 +34,13 @@ public class Main {
             e.printStackTrace();
         }
     }
-// takes a user input
+    // takes a user input
     public static String userInput() {
         Scanner input = new Scanner(System.in);
         String UserInput = "";
 
         try {
-            UserInput = input.next();
+            UserInput = input.nextLine();
 
         } catch (Exception e) {
             System.out.println("an error occurred " + e);
@@ -55,14 +49,16 @@ public class Main {
 
         return (UserInput);
     }
-// wirter user inputs to the file
+    // writes user inputs to the file
     public static void WriteToFile(ArrayList<ArrayList<String>> books ) {
         try {
-            FileWriter myWriter = new FileWriter(libary.getName(), true); //True means append to file contents, False means overwrite
+            FileWriter myWriter = new FileWriter(library.getName(), true); //True means append to file contents, False means overwrite
             System.out.println("This is the contents of the file:");
             // Overwrites everything in the file
             for (int i = 0; i < books.toArray().length; i++) {
                 for (int j = 0; j < books.get(i).toArray().length; j++) {
+                    System.out.println(i);
+                    System.out.println(j);
                     myWriter.write(books.get(i).get(j)+"+ ");
                 }
                 myWriter.write("\n");
@@ -76,12 +72,27 @@ public class Main {
             e.printStackTrace();
         }
     }
-// let the user add books to the 2d array list
-    public static ArrayList<ArrayList<String>> addBooks(ArrayList<ArrayList<String>> books){
+    // let the user add books to the 2d array list
+    public static void addBooks(ArrayList<ArrayList<String>> books){
         System.out.println("number of books");
-        int booksNum = Integer.parseInt(userInput());
+        int booksNum;
+        while(true) {
+            try {
+                booksNum = Integer.parseInt(userInput());
+                if(booksNum>0){
+                    break;
+                }
+                else{
+                    System.out.println("number to be greater than 0");
+                }
+            }
+            catch(Exception e){
+                System.out.println("invalid input "+e);
+            }
+        }
         for (int i = 0; i < booksNum; i++) {
             ArrayList<String> currentBook=new ArrayList<>();
+
             System.out.println("Book title");
             currentBook.add(userInput());
 
@@ -94,23 +105,24 @@ public class Main {
             System.out.println("genre");
             currentBook.add(userInput());
 
+
             books.add(currentBook);
         }
-        return books;
+
 
     }
-// deleates the file
+    // delete the file
     public static void DeleteFile() {
-        if (libary.delete()) {
-            System.out.println("Deleted the file: " + libary.getName());
+        if (library.delete()) {
+            System.out.println("Deleted the file: " + library.getName());
         } else {
             System.out.println("Failed to delete the file.");
         }
     }
-// reads line in a file
+    // reads line in a file
     public static void ReadFile() {
         try {
-            Scanner myReader = new Scanner(libary);
+            Scanner myReader = new Scanner(library);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
                 System.out.println(data);
@@ -121,6 +133,48 @@ public class Main {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+    }
+
+    public static void mainMenu(){
+        while(true){
+            System.out.println("options are 1:read file 2:add book 3:delete file 4:exit");
+            int option;
+            while(true) {
+                try {
+                    option = Integer.parseInt(userInput());
+                    if ((option > 0) && (option < 5)) {
+                        break;
+                    } else {
+                        System.out.println("number to be greater than 0 and less than 5");
+                    }
+                } catch (Exception e) {
+                    System.out.println("invalid input " + e);
+                }
+            }
+
+            if (option == 1){
+                ReadFile();
+            }
+            if(option == 2){
+                addBooks(books);
+                WriteToFile(books);
+
+            }
+            if(option == 3){
+                System.out.println("Do you want to delete this file now Y or N?");
+                String userOption = userInput();
+                if (userOption.equalsIgnoreCase("y")) {
+                    DeleteFile();
+                    CreateFile();
+                }
+            }
+            if(option == 4){
+                break;
+            }
+
+        }
+
+
     }
 
 }
